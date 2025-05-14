b368c4c16398adbdae51b130e21630d02e7b16e0
azmusweb <azmusweb@gmail.com>
2025-05-14 12:44:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-// Mengambil data dari data.json dan menampilkannya di index.html
-fetch('data.json')
-    .then(response => response.json())
+fetch('https://docs.google.com/spreadsheets/d/1oMHeKOF2_D6deuV8T1l10_GB0wgsPGLV7WrPcJ6Qxww/tq?sheet=Sheet1')
+    .then(response => response.text())
     .then(data => {
+        const json = JSON.parse(data.substr(47).slice(0, -2));
+        const rows = json.table.rows;
         const container = document.getElementById('data-container');
-        data.forEach(item => {
+        
+        rows.forEach(row => {
+            const title = row.c[0].v; // Judul
+            const description = row.c[1].v; // Deskripsi
+            
             const div = document.createElement('div');
-            div.innerHTML = `<h3>${item.title}</h3><p>${item.description}</p>`;
+            div.innerHTML = `<h3>${title}</h3><p>${description}</p>`;
             container.appendChild(div);
         });
     })
